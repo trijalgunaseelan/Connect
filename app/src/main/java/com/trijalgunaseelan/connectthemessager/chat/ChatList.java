1e6d7145690be2a39bb631a9076620d80235329e
Trijal P G <123310854+trijalgunaseelan@users.noreply.github.com>
2023-09-22 11:20:17 +0530

    hi

---
+++ app/src/main/java/com/trijalgunaseelan/connectthemessager/chat/ChatList.java
@@ -0,0 +1,28 @@
+package com.trijalgunaseelan.connectthemessager.chat;
+public class ChatList {
+
+    private final String mobile, message, date, time;
+
+    public ChatList(String mobile, String message, String date, String time) {
+        this.mobile = mobile;
+        this.message = message;
+        this.date = date;
+        this.time = time;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+}
